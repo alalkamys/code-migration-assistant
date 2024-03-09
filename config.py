e665e5c94abccde83e30b1b3ee1070ccfa8f95d8
alalkamys <essamshehab33_3@h-eng.helwan.edu.eg>
2024-03-09 16:23:23 +0200

    feat: add LOGGING_CONFIG config property
    This change is to add a centralized logging config dictionary for `code_migration_assistant` logger

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+from utils import filters
+
+import logging
 import os
 
 
@@ -7,6 +10,53 @@ class AppConfig:
 
     TARGETS_CONFIG_FILE = os.getenv(
         'CODE_MIGRATION_ASSISTANT_TARGETS_CONFIG_FILE', "./config.json")
+
+    LOGGING_CONFIG = {
+        'version': 1,
+        'disable_existing_loggers': True,
+        'filters': {
+            'info_lvl_filter': {
+                '()': filters.SingleLevelFilter,
+                'passlevel': logging.INFO,
+                'reject': False
+            },
+            'info_lvl_filter_inverter': {
+                '()': filters.SingleLevelFilter,
+                'passlevel': logging.INFO,
+                'reject': True
+            }
+        },
+        'formatters': {
+            'default': {
+                'format': '[%(levelname)s]:%(name)s:%(asctime)s, %(message)s',
+            }
+        },
+        'handlers': {
+            'stdout_handler': {
+                'class': 'logging.StreamHandler',
+                'formatter': 'default',
+                'stream': 'ext://sys.stdout',
+                'filters': ['info_lvl_filter']
+            },
+            'stderr_handler': {
+                'class': 'logging.StreamHandler',
+                'formatter': 'default',
+                'stream': 'ext://sys.stderr',
+                'filters': ['info_lvl_filter_inverter']
+            },
+        },
+        'loggers': {
+            APP_NAME: {
+                'handlers': ['stdout_handler', 'stderr_handler'],
+                'level': 'INFO',
+                'propagate': False,
+            },
+        },
+        'root': {
+            'level': 'INFO',
+            'handlers': ['stdout_handler', 'stderr_handler'],
+        }
+    }
 
 
 app_config = AppConfig
