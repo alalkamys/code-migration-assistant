d95055048d44044442ca48e21eb69d3846dc55b7
alalkamys <essamshehab33_3@h-eng.helwan.edu.eg>
2024-03-09 17:20:14 +0200

    feat: code migration assistant identity setup

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from config import app_config
+from utils.helpers import identity_setup
 from utils.helpers import load_targets_configs
 from utils.helpers import load_target_repos
 
 import logging.config
+import os
 
 if __name__ == "__main__":
     logging.config.dictConfig(app_config.LOGGING_CONFIG)
@@ -15,4 +17,14 @@ if __name__ == "__main__":
         file_path=app_config.TARGETS_CONFIG_FILE)
     TARGET_REPOS = load_target_repos(repos=TARGETS_CONFIG['targetRepos'])
 
-    _logger.info([repo.working_dir for repo in TARGET_REPOS])
+    if len(TARGET_REPOS) > 0:
+        _logger.info(f"{len(TARGET_REPOS)} target repo(s) found")
+        _logger.info("Initiating code migration assistant program..")
+        for repo in TARGET_REPOS:
+            repo_name = os.path.basename(
+                os.path.normpath(repo.working_tree_dir))
+
+            _logger.info(f"Migrating {repo_name}..")
+
+            identity_setup(
+                repo=repo, actor_username=app_config.ACTOR['username'], actor_email=app_config.ACTOR['email'])
--- utils/helpers.py
+++ utils/helpers.py
@@ -5,6 +5,7 @@ from git.exc import GitCommandError
 from git.exc import NoSuchPathError
 from typing import Any
 from typing import List
+from typing import Type
 import json
 import logging
 import os
@@ -52,3 +53,12 @@ def load_target_repos(repos: List[dict]) -> List[Repo]:
             _logger.error(f"Unexpected error when loading '{
                           repo['name']}': {str(e)}")
     return result
+
+
+def identity_setup(repo: Type[Repo], actor_username: str, actor_email: str) -> None:
+    config_writer = repo.config_writer()
+    _logger.debug(f"Setting username to {actor_username}")
+    config_writer.set_value('user', 'name', actor_username).release()
+    _logger.debug(f"Setting email to <{actor_email}>")
+    config_writer.set_value('user', 'email', actor_email).release()
+    del (config_writer)
