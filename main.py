486205998d7fab376d43a5dafaa5f955d7d93632
alalkamys <essamshehab33_3@h-eng.helwan.edu.eg>
2024-03-09 17:08:51 +0200

    feat: load target repos
    Adding `load_target_repos()` in `utils/helpers.py` to load the target repos in a list of git.Repo objects for future management

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from config import app_config
 from utils.helpers import load_targets_configs
+from utils.helpers import load_target_repos
 
 import logging.config
 
@@ -12,5 +13,6 @@ if __name__ == "__main__":
 
     TARGETS_CONFIG = load_targets_configs(
         file_path=app_config.TARGETS_CONFIG_FILE)
+    TARGET_REPOS = load_target_repos(repos=TARGETS_CONFIG['targetRepos'])
 
-    _logger.info(TARGETS_CONFIG)
+    _logger.info([repo.working_dir for repo in TARGET_REPOS])
--- utils/helpers.py
+++ utils/helpers.py
@@ -1,8 +1,13 @@
 from config import app_config
 
+from git import Repo
+from git.exc import GitCommandError
+from git.exc import NoSuchPathError
 from typing import Any
+from typing import List
 import json
 import logging
+import os
 import sys
 
 _logger = logging.getLogger(app_config.APP_NAME)
@@ -21,3 +26,29 @@ def load_targets_configs(file_path: str) -> dict[str, Any]:
             file_path}'. Invalid JSON format."
     _logger.error(error_message)
     sys.exit(1)
+
+
+def load_target_repos(repos: List[dict]) -> List[Repo]:
+    result = []
+    for repo in repos:
+        try:
+            result.append(Repo(path=repo['source']) if repo['type'].lower() == "local" else Repo.clone_from(
+                url=repo['source'], to_path=f"{app_config.REMOTE_TARGETS_CLONING_PATH}/{repo['name']}"))
+        except GitCommandError as git_cmd_err:
+            if git_cmd_err.status == 128 and 'already exists' in git_cmd_err.stderr:
+                cwd = os.getcwd()
+                repo_absolute_path = os.path.abspath(os.path.join(
+                    cwd, f"{app_config.REMOTE_TARGETS_CLONING_PATH}/{repo['name']}"))
+                _logger.info(f"'{repo_absolute_path}' already exists, using..")
+                result.append(
+                    Repo(path=f"{app_config.REMOTE_TARGETS_CLONING_PATH}/{repo['name']}"))
+            else:
+                _logger.error(f"Unexpected GitCommandError: {
+                              str(git_cmd_err)}")
+        except NoSuchPathError:
+            _logger.error(f"Invalid 'Remote' repo URL '{
+                          repo['source']}' no such path. Check '{repo['name']}' source URL")
+        except Exception as e:
+            _logger.error(f"Unexpected error when loading '{
+                          repo['name']}': {str(e)}")
+    return result
